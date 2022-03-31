7c5b26e04f83e4f9dc1f6a2529b20c99516193a1
Flukus99 <elementary@outlook.es>
2022-03-31 17:53:08 -0500

    cors3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs=require("fs");
 const app=express();
 const PORT= process.env.PORT || 8080;
 const date=Date.now()
-const whitelist=["http://localhost:4200","https://mi-anthro.vercel.app"]
+const whitelist=["http://localhost:4200","https://mi-anthro.vercel.app","https://mi-anthro.vercel.app/home"]
 
 const storage=multer.diskStorage({
     destination: 'uploads/',
